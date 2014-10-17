f6af1687e52ba5c5ba7b7a94a1fb752a47ea7731
Mark <mark.van.lagen@gmail.com>
2014-10-17 17:24:49 +0200

    Initial Push

---
+++ kicktraq-safari-extension.js
@@ -0,0 +1,46 @@
+"use strict";
+
+jQuery(function() {
+	var currentPath = document.location.pathname;
+	var project = "";
+	var imageToLoad = "";
+	
+	var contentDiv = document.getElementById("content");
+	var image = document.createElement("img");
+	var imageLink = document.createElement("a");
+	var imageContainer = document.createElement("div");
+	var placeholder = document.createElement("span");
+	placeholder.innerHTML = "(Loading your very own snazzy kicktraq chart)";
+	placeholder.style.color = "gray";
+	placeholder.style.lineHeight = "270px";
+	
+	imageContainer.id = "kicktraq"
+	imageContainer.style.textAlign = "center";
+	imageContainer.style.height = "270px";
+	imageContainer.style.width = "940px";
+	imageContainer.style.margin = "auto";
+	imageContainer.style.marginBottom = "10px";
+	
+	imageContainer.appendChild(placeholder);
+	contentDiv.insertBefore(imageContainer, contentDiv.firstChild);
+	
+	if(currentPath.indexOf("/posts") != -1 || currentPath.indexOf("/backers") != -1 || currentPath.indexOf("/comments") != -1) {
+		project = currentPath.substr(0, currentPath.lastIndexOf("/") + 1);
+		if(currentPath.indexOf("/posts") != -1 || currentPath.indexOf("/comments") != -1) {
+			imageToLoad = "//www.kicktraq.com" + project + "/dailycomments.png";
+		} else if(currentPath.indexOf("/backers") != -1) {
+			imageToLoad = "//www.kicktraq.com" + project + "/backerchart.png";
+		}
+	} else {
+		project = currentPath;
+		imageToLoad = "//www.kicktraq.com" + project + "/dailychart.png";
+	}
+	
+	image.src = imageToLoad;
+	$(image).load(function() {
+		imageLink.href = "//www.kicktraq.com" + project;
+		imageLink.appendChild(image);
+		imageContainer.removeChild(placeholder);
+		imageContainer.appendChild(imageLink);
+	})
+});
